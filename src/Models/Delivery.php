289ae54e6b2e2dde73582aa0c0952a8a8190699e
railken <railken@protonmail.ch>
2020-01-19 18:27:22 +0100

    fix namespaces

--- src/Models/Delivery.php
+++ src/Models/Delivery.php
@@ -4,7 +4,7 @@ namespace Amethyst\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Amethyst\Common\ConfigurableModel;
+use Amethyst\Core\ConfigurableModel;
 use Railken\Lem\Contracts\EntityContract;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
